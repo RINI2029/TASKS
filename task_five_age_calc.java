778003513f3f9897482a1ffada2d7edbdb5483df
RINI2029 <144047576+RINI2029@users.noreply.github.com>
2024-09-27 18:40:33 +0530

    Add files via upload

---
+++ task_five_age_calc.java
@@ -0,0 +1,20 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+
+public class task_five_age_calc
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your birthdate (yyyy-mm-dd): ");
+        String birthdateInput = scanner.nextLine();
+        LocalDate birthdate = LocalDate.parse(birthdateInput);
+        LocalDate currentDate = LocalDate.now();
+        int age = Period.between(birthdate, currentDate).getYears();
+        System.out.println("Your age is: " + age);
+
+        scanner.close();
+    }
+}
+
