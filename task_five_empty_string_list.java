1b2fa370657d57cd088b4198b62b740fb83c063e
RINI2029 <144047576+RINI2029@users.noreply.github.com>
2024-09-27 18:40:06 +0530

    Add files via upload

---
+++ task_five_empty_string_list.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class task_five_empty_string_list
+{
+    public static void main(String[] args)
+    {
+
+        List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd", "", "jkl");
+
+        List<String> nonEmptyStrings = strings.stream()
+                .filter(str -> !str.isEmpty())
+                .collect(Collectors.toList());
+
+        System.out.println("Non-empty strings: " + nonEmptyStrings);
+    }
+}
